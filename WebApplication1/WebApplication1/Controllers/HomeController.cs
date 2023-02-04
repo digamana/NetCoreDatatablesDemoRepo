8d6004995356e18f94011597f7195b0266ad587a
User <User@DESKTOP-LF7SA0P>
2023-02-04 20:21:33 +0800

    initial

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult ShowDataTables()
+        {
+            Device device = new Device();
+            return View(device);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public ActionResult GetDevices()
+        {
+            var result = new List<Device>();
+            result.Add(new Device { Id = 1, ItemDescription = "華碩", ItemName = "電腦" });
+            result.Add(new Device { Id = 2, ItemDescription = "蘋果", ItemName = "手機" });
+            return Json(result);
+        }
+        [HttpGet]
+        public ActionResult Edit(string temp)
+        {
+            var result = new List<Device>();
+            result.Add(new Device { Id = 1, ItemDescription = "華碩", ItemName = "電腦" });
+            result.Add(new Device { Id = 2, ItemDescription = "蘋果", ItemName = "手機" });
+            var result2 = result.Where(c => c.ItemDescription == temp).FirstOrDefault();
+
+            return PartialView("_PartialViewEdit", result2);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Device DevicesDto)
+        {
+            return Json("");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication1.Models
+{
+    public class Device
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+        [DisplayName("類型")]
+        public int DeviceTypeId { get; set; }
+        [DisplayName("名稱")]
+        public string ItemName { get; set; }
+        [DisplayName("物品描述")]
+        public string ItemDescription { get; set; }
+    }
+}
